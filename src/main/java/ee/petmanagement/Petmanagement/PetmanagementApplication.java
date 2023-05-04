e31378912f967a607ed2e7510a777874bdff5fc1
margohu <marxhytt@gmail.com>
2023-05-04 12:08:50 +0300

    Initial commit

---
+++ src/main/java/ee/petmanagement/Petmanagement/PetmanagementApplication.java
@@ -0,0 +1,13 @@
+package ee.petmanagement.Petmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/petmanagement/Petmanagement/PetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.petmanagement.Petmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
